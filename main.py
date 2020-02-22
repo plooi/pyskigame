28dd7063098cdb95bfd90efda178cc2f3468f901
Peter Looi <peterlooi11@gmail.com>
2020-02-22 01:57:26 -0800

    automatically makes save and world directory in ..

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pylooiengine
 from pylooiengine import *
 w = Window(internal_size = (2000,1080), fps=30)
 #import mash_textures
+import os
 import pygame
 
 import rooms
 
 def main():  
+    if not os.path.exists("../worlds"):os.mkdir("../worlds")
+    if not os.path.exists("../saves"):os.mkdir("../saves")
     w.start()
 if __name__ == "__main__": main()
